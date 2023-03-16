e32519556894b0ca6c47418399703601e44ec0c0
Jonas Neubürger <jonas793@gmail.com>
2023-03-16 13:01:18 +0100

    fix logging

--- src/tracker.py
+++ src/tracker.py
@@ -8,15 +8,16 @@ from helpers import parse_page, Logger, LogLevel
 
 DATA_LOCATION = '../data/data.csv'
 LOG_LOCATION = '../logs/tracker_log.log'
-LOGGER = Logger(LOG_LOCATION)
+
 
 def main():
+    logger = Logger(LOG_LOCATION)
     url = 'https://www.einmalzahlung200.de/eppsg-de'
     # Get the page
     page = requests.get(url)
     status = page.status_code
     if status != 200:
-        LOGGER.log('tracker', LogLevel.ERROR, 'Could not get the page!')
+        logger.log('tracker', LogLevel.ERROR, 'Could not get the page!')
         raise Exception('Could not get the page')
 
     parsed = parse_page(page)
@@ -30,7 +31,7 @@ def main():
     d.paid_out_timestamp = parsed[4]
    
     d.to_csv()
-    LOGGER.log('tracker', LogLevel.INFO, 'Done!')
+    logger.log('tracker', LogLevel.INFO, 'Done!')
 
 class data:
     def __init__(self):
@@ -40,6 +41,7 @@ class data:
         self.paid_out:(np.int64 | None) = None
         self.paid_out_sum:(np.int64 | None) = None
         self.paid_out_timestamp:(datetime.datetime | None) = None
+        self.logger = Logger(LOG_LOCATION)
 
     def __str__(self):
         return f'{self.timestamp} | Anträge gestellt: {self.successful_count} ({self.succesful_timestamp}) | Anträge ausgezahlt: {self.paid_out}, Gesamt: {self.paid_out_sum}€ ({self.paid_out_timestamp})'
@@ -58,22 +60,22 @@ class data:
         # create folder if it does not exist
         if not os.path.exists('../data'):
             os.mkdir('../data')
-            LOGGER.log('tracker', LogLevel.INFO, 'Created data folder at ../data')
+            self.logger.log('tracker', LogLevel.INFO, 'Created data folder at ../data')
             
         df = pd.DataFrame([self.__dict__()])
         if not os.path.exists(DATA_LOCATION):
             df.to_csv(DATA_LOCATION, index=False)
-            LOGGER.log('tracker', LogLevel.INFO, 'Created data csv at {DATA_LOCATION}')
+            self.logger.log('tracker', LogLevel.INFO, 'Created data csv at {DATA_LOCATION}')
         else:
             # check if the data is already in the csv
             df_csv = pd.read_csv(DATA_LOCATION)
             df_csv['succesful_timestamp'] = pd.to_datetime(df_csv['succesful_timestamp'])
             df_csv['paid_out_timestamp'] = pd.to_datetime(df_csv['paid_out_timestamp'])
             if self.succesful_timestamp in df_csv['succesful_timestamp'].values and self.paid_out_timestamp in df_csv['paid_out_timestamp'].values:
-                LOGGER.log('tracker', LogLevel.INFO, 'Data already in csv. Skipping...')
+                self.logger.log('tracker', LogLevel.INFO, 'Data already in csv. Skipping...')
                 return
             pd.concat([pd.read_csv(DATA_LOCATION), df], ignore_index=True).to_csv(DATA_LOCATION, index=False)
-            LOGGER.log('tracker', LogLevel.INFO, 'Wrote data to csv.')
+            self.logger.log('tracker', LogLevel.INFO, 'Wrote data to csv.')
     
     def to_json(self):
         return self.__dict__()
