52ed685ed7fa767197c0499d1f8e4332aa718b0c
Jonas Neubürger <jonas793@gmail.com>
2023-03-16 11:14:30 +0100

    add requirements and setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='nothilfe_tracker',
+      version='0.1',
+      description='A tracker for the einmalzahlung200.de counters',
+      author='Jonas Neubürger',
+      author_email='jonas.neubuerger005@stud.fh-dortmund.de',
+      packages=find_packages(include=['src', 'src.*']),
+     )
